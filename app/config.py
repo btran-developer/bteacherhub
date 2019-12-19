04af41abbe29159f9272e8c93b8d1ff7e86b1de7
Bill Tran <btran.developer@gmail.com>
2019-12-19 12:04:18 -0600

    prepare for deploy

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'the-world-most-secret-key'
-    REPORTS_PER_PAGE = 2
+    REPORTS_PER_PAGE = os.environ.get('REPORTS_PER_PAGE', 10)
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
